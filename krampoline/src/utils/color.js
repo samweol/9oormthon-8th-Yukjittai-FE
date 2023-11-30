650607caca7c78fa36c64e11e0416f8b45fa7571
samweol <sejuyes@gmail.com>
2023-11-30 10:08:59 +0900

    CHORE: 공통 유틸 파일 생성

---
+++ krampoline/src/utils/color.js
@@ -0,0 +1,12 @@
+export const COLOR = {
+  "btn-primary": "#FF8743",
+  "btn-disabled": "D6D6D6",
+
+  "font-black": "#000000",
+  "font-gray-300": "#87919B",
+  "font-gray-900": "#656565",
+  "font-white": "#FFFFFF",
+
+  "bg-gray-500": "#F6F6F6",
+  "bg-primary": "##FFFFFF",
+};
---
+++ krampoline/src/utils/fontSize.js
@@ -0,0 +1,3 @@
+export const FONT_SIZE = {
+  m: "16px",
+};
